b06f83252cecae4623f7b62bc3e4281582af0058
Carlos Mezquita <mezquitalobato@gmail.com>
2023-03-10 22:55:24 +0100

    added daily question module

---
+++ plugins/dailyQuestion.plugin.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const cron = require('node-cron');
+const api = require("../core/api/api.js")
+
+const fileName = "./storage/questions-dataset.json"
+const channelID = "914557391067054081"
+const dailyPingID = "711962609523621960"
+
+module.exports = () => {
+    const { client } = api
+
+    const channel = client.channels.cache.get(channelID);
+
+    cron.schedule('0 14 * * *', () => {
+        sendRandomDailyQuestion(channel);
+    }, {
+        scheduled: true,
+        timezone: "Europe/Madrid"
+    });
+}
+
+function sendRandomDailyQuestion(channel) {
+    const questions = JSON.parse(fs.readFileSync(fileName, "utf8"));
+
+    const randomIndex = Math.floor(Math.random() * questions.read_questions.length);
+    const randomQuestion = questions.read_questions[randomIndex];
+
+    channel.send({
+        "content": `<@&${dailyPingID}>`,
+        "embeds": [
+            {
+                "color": 39129,
+                "timestamp": new Date().toISOString(),
+                "author": {
+                    "icon_url": "https://cdn-icons-png.flaticon.com/512/5893/5893002.png",
+                    "name": "Pregunta Diaria"
+                },
+                "footer": {
+                    "text": "Preguntas de la Comunidad"
+                },
+                "title": randomQuestion
+            }
+        ]
+    });
+
+    questions.read_questions.splice(randomIndex, 1);
+    questions.unread_questions.push(randomQuestion);
+    const jsonString = JSON.stringify(questions, null, 2);
+
+    fs.writeFileSync(fileName, jsonString, "utf8");
+}
