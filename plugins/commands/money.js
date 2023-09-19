eaf7b3253892c30dae2681901c88e233f84f8f05
Carlos Mezquita <mezquitalobato@gmail.com>
2023-09-20 01:42:03 +0200

    chore: remove unnecessary error catch

--- plugins/commands/money.js
+++ plugins/commands/money.js
@@ -34,15 +34,7 @@ module.exports = {
 		
 			const subcommand = subcommands.get(interaction.options.getSubcommand());
 		
-			try {
-				await subcommand.execute(interaction);
-			} catch (error) {
-				console.error(error);
-				if (interaction.replied || interaction.deferred) {
-					await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-				} else {
-					await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-				}
-			}
+			await subcommand.execute(interaction);
+
 	},
 };
